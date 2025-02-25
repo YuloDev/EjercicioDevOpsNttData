2bbecff6a024b3ef857cde5092edc19b300d50f7
Rafael Piedra <rafael.piedra@nextisolutions.com>
2025-02-25 09:45:10 -0500

    fix: corrección en balanceador de carga

--- terraform/load_balancer.tf
+++ terraform/load_balancer.tf
@@ -3,8 +3,8 @@ resource "aws_security_group" "alb_sg" {
   vpc_id = aws_vpc.main.id
 
   ingress {
-    from_port   = 443
-    to_port     = 443
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -59,17 +59,6 @@ resource "aws_lb_listener" "alb_listener" {
 }
 
 
-resource "aws_lb_listener" "alb_listener_https" {
-  load_balancer_arn = aws_lb.alb.arn
-  port              = "443"
-  protocol          = "HTTPS"
 
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = null  
 
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.ecs_tg.arn
-  }
-}
 
