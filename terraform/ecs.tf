0692f26eb622ed04052cc785dbd8b8b839485ef4
Rafael Piedra <rafael.piedra@nextisolutions.com>
2025-02-25 17:18:24 -0500

    fix: corrección rol

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -13,6 +13,7 @@ resource "aws_ecs_task_definition" "task" {
   requires_compatibilities = ["FARGATE"]
   cpu                      = "256"
   memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_execution_role.arn
 
   container_definitions = jsonencode([{
     name  = "nodejs-app"
