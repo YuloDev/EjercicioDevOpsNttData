2b677bd74428ce1a8bbf29bee57049a431bb129c
Rafael Piedra <rafael.piedra@nextisolutions.com>
2025-06-02 10:40:32 -0500

    Fix: ajuste con nueva cuenta

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -1,8 +1,3 @@
-resource "aws_ecr_repository" "prueba_repository" {
-  name = "prueba-repository"
-}
-
-
 resource "aws_ecs_cluster" "cluster" {
   name = "cluster-ecs"
 }
@@ -15,38 +10,30 @@ resource "aws_ecs_task_definition" "task" {
   memory                   = "512"
   execution_role_arn       = aws_iam_role.ecs_execution_role.arn
 
-
   container_definitions = jsonencode([{
     name  = "nodejs-app"
-    image = "${aws_ecr_repository.prueba_repository.repository_url}"
+    image = aws_ecr_repository.prueba_repository.repository_url
     portMappings = [{
       containerPort = 8080
-      hostPort      = 8080
       protocol      = "tcp"
     }]
-
   }])
 }
 
+resource "aws_ecr_repository" "prueba_repository" {
+  name = "prueba-repository"
+}
 
 resource "aws_ecs_service" "servicio" {
   name            = "app-service"
   cluster         = aws_ecs_cluster.cluster.id
   task_definition = aws_ecs_task_definition.task.arn
-  desired_count   = 2
+  desired_count   = 1
   launch_type     = "FARGATE"
 
   network_configuration {
-    security_groups  = [aws_security_group.ecs_app_sg.id]
-    subnets          = data.aws_subnets.selected.ids
+    subnets         = data.aws_subnets.selected.ids
+    security_groups = [aws_security_group.ecs_app_sg.id]
     assign_public_ip = true
   }
-
-  load_balancer {
-    target_group_arn = aws_lb_target_group.ecs_tg.arn
-    container_name   = "nodejs-app"
-    container_port   = 8080 
-  }
 }
-
-
--- terraform/network.tf
+++ terraform/network.tf
@@ -1,5 +1,65 @@
-data "aws_vpc" "main" {
-  id = "vpc-0ad35e8ee40d0d6b8"
+# vpc.tf
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "main-vpc"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main-gateway"
+  }
+}
+
+resource "aws_subnet" "public1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "${var.region}a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet-1"
+  }
+}
+
+resource "aws_subnet" "public2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "${var.region}b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet-2"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.public1.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "b" {
+  subnet_id      = aws_subnet.public2.id
+  route_table_id = aws_route_table.public.id
 }
 
 
@@ -11,14 +71,14 @@ resource "aws_security_group" "alb_app_sg" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
     from_port   = 0
     to_port     = 0
-    protocol    = "-1"  
-    cidr_blocks = ["0.0.0.0/0"] 
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
@@ -30,15 +90,15 @@ resource "aws_security_group" "ecs_app_sg" {
   egress {
     from_port   = 0
     to_port     = 0
-    protocol    = "-1"  
-    cidr_blocks = ["0.0.0.0/0"] 
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] 
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
