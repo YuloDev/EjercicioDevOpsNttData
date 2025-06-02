3f388ef5e4d3635bf4e32bc95056ca5853b9e002
Rafael Piedra <rafael.piedra@nextisolutions.com>
2025-06-02 11:53:28 -0500

    healthcheck

--- terraform/load_balancer.tf
+++ terraform/load_balancer.tf
@@ -17,14 +17,14 @@ resource "aws_lb_target_group" "ecs_tg" {
 
 
   health_check {
-    path                = "/"
-    protocol            = "HTTP"
-    matcher             = "200-399"
+    path                = "/health"
     interval            = 30
-    timeout             = 5
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
+    timeout             = 10
+    healthy_threshold   = 3
+    unhealthy_threshold = 5
+    matcher             = "200"
   }
+
 }
 
 
